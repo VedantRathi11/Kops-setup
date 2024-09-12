6db6d47417e7a4c6415e2460f1260cdcba72c4bf
Vedant Rathi <160308562+VedantRathi11@users.noreply.github.com>
2024-09-12 15:49:23 +0530

    Update kops-setup.sh

--- kops-setup.sh
+++ kops-setup.sh
@@ -6,7 +6,7 @@
 #! /bin/bash
 aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.29.0/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket vedantrathi11.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket vedantrathi11.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://vedantrathi11.k8s.local
-kops create cluster --name vedant.k8s.local --zones us-east-1a --control-plane-count=1 --control-plane-size t2.medium --node-count=2 --node-size t2.micro
+kops create cluster --name vedant.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name vedant.k8s.local --yes --admin
